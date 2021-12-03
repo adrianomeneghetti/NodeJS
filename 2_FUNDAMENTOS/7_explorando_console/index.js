45f8bc761fd573f39304f280b60ff671b22441f7
Adriano Meneghetti <adrianormeneghetti@gmail.com>
2021-12-03 14:50:19 -0300

    Explorando o console

---
+++ 2_FUNDAMENTOS/7_explorando_console/index.js
@@ -0,0 +1,22 @@
+//masis de um valor
+
+const x = 10
+const y = 'Adriano'
+const z = [1, 2]
+
+console.log(x,y,z)
+
+//contagem de impressões
+console.count(`o valor de x é: ${x}, contagem`)
+console.count(`o valor de x é: ${x}, contagem`)
+console.count(`o valor de x é: ${x}, contagem`)
+console.count(`o valor de x é: ${x}, contagem`)
+console.count(`o valor de x é: ${x}, contagem`)
+
+// variável entre string
+console.log('O nome é, %s, e ele é programador',  y)
+
+//limpar console
+setTimeout(() =>{
+    console.clear()
+}, 2000)
